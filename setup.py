7e183ed04b2438d63b948ca62ffd8f694e0a0430
Vitaly Haritonsky <vharitonsky@gmail.com>
2015-03-14 13:08:22 +0200

    revert setup

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    name = 'Hello',
-    ext_modules = cythonize("hello.pyx"),
+    name = 'Clope clustering',
+    ext_modules = cythonize("clope.pyx"),
 )
